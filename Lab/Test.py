e61ae7a1c319be50870c8d5791be491426382d47
Antti Staalnacke <astalnacke@gmail.com>
2014-12-06 10:25:13 +0100

    Added some fibonchi

--- Lab/Test.py
+++ Lab/Test.py
@@ -10,3 +10,14 @@ A = 5
 B = 3
 C = A + B
 print 'A + B is', C
+
+
+
+def fib(n):    # write Fibonacci series up to n
+    "Print a Fibonacci series up to n"
+    a, b = 0, 1
+    while b < n:
+        print b,
+        a, b = b, a+b
+
+fib(7)
